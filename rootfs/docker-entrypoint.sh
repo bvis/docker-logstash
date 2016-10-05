18ff862ce7dba20c79cb1b984791e4ae1eab05e1
Riccardo Piccoli <riccardo.piccoli@softonic.com>
2016-10-05 10:09:43 +0200

    improve init script so that can accept protocol too

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
-set -e
+#!/bin/bash -e
 
 # Replace placeholder by environment variables
-sed -i "s/#ELASTICSEARCH#/${ELASTICSEARCH_ADDR}:${ELASTICSEARCH_PORT}/g" /config-dir/logstash.conf
+sed -i "s@#ELASTICSEARCH#@${ELASTICSEARCH_ADDR}:${ELASTICSEARCH_PORT}@g" /config-dir/logstash.conf
 
 # Add logstash as command if needed
 if [ "${1:0:1}" = '-' ]; then
